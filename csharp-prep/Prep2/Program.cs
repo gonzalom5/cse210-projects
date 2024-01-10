c0ec270b05a868b8d6a4de99fe0d853fad84624e
Gonzalo <mar17004@byui.edu>
2024-01-10 14:49:25 -0300

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if(percent >= 60 && percent < 100 && (percent % 10) < 3)
+        {
+            letter += "-";
+        }
+        else if (percent >= 60 && percent < 90 && (percent % 10) >= 7)
+        {
+            letter += "+";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+         
+         if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Do not Give up and Keep Trying!");
+        }
+    }   
+    
 }
